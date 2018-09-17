ef3524da6778743be2ab5d8a709887623c6d757e
Daiki Kawanuma <KAWANUMA@jp.ibm.com>
2018-09-17 20:55:38 +0900

    Swagger 定義追加

--- surfing-judge/src/main/java/com/projectrespite/surfingjudge/application/controller/ScoreController.java
+++ surfing-judge/src/main/java/com/projectrespite/surfingjudge/application/controller/ScoreController.java
@@ -2,6 +2,9 @@ package com.projectrespite.surfingjudge.application.controller;
 
 import com.projectrespite.surfingjudge.domain.model.response.ScoreResponse;
 import com.projectrespite.surfingjudge.domain.service.ScoreService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,14 +13,16 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(tags = "スコア", description = "スコアを取得する")
 @AllArgsConstructor
 public class ScoreController {
 
     private ScoreService service;
 
     @GetMapping("/scores/{round}/{heat}")
-    public List<ScoreResponse> getScores(@PathVariable int round,
-                                         @PathVariable int heat) {
+    @ApiOperation(value = "画像取得", produces = "application/json", response = ScoreResponse.class, responseContainer = "List")
+    public List<ScoreResponse> getScores(@ApiParam(value = "ラウンド", required = true) @PathVariable int round,
+                                         @ApiParam(value = "ヒート", required = true) @PathVariable int heat) {
 
         return service.getScores(round, heat);
     }
