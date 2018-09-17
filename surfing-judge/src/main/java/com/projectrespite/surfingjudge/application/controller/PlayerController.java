a6dabe751ddef5562a9177daa8aa35c7675b2870
Daiki Kawanuma <KAWANUMA@jp.ibm.com>
2018-09-17 18:05:17 +0900

    Swagger 定義を追加

--- surfing-judge/src/main/java/com/projectrespite/surfingjudge/application/controller/PlayerController.java
+++ surfing-judge/src/main/java/com/projectrespite/surfingjudge/application/controller/PlayerController.java
@@ -2,6 +2,8 @@ package com.projectrespite.surfingjudge.application.controller;
 
 import com.projectrespite.surfingjudge.domain.model.response.PlayerResponse;
 import com.projectrespite.surfingjudge.domain.repository.IPlayerRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Api(tags = "プレイヤー", description = "プレイヤーを取得する")
 @AllArgsConstructor
 public class PlayerController {
 
     private IPlayerRepository repository;
 
     @GetMapping(value = "/players")
+    @ApiOperation(value = "プレイヤーを取得する", response = PlayerResponse.class, responseContainer = "List")
     public List<PlayerResponse> getPlayers() {
 
         return repository.getPlayers();
