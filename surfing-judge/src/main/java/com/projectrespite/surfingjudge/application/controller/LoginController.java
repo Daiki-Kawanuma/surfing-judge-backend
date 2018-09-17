102d6e07eee553e3e53227662ca35d6fc96ed112
Daiki Kawanuma <KAWANUMA@jp.ibm.com>
2018-09-17 17:20:33 +0900

    APPLICATION_JSON_UTF8_VALUE に変更

--- surfing-judge/src/main/java/com/projectrespite/surfingjudge/application/controller/LoginController.java
+++ surfing-judge/src/main/java/com/projectrespite/surfingjudge/application/controller/LoginController.java
@@ -3,6 +3,9 @@ package com.projectrespite.surfingjudge.application.controller;
 import com.projectrespite.surfingjudge.domain.model.request.LoginRequest;
 import com.projectrespite.surfingjudge.domain.model.response.LoginResponse;
 import com.projectrespite.surfingjudge.domain.service.LoginService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ResponseHeader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@Api(tags = "ログイン", description = "ログインする")
 public class LoginController {
 
     @Autowired
     LoginService service;
 
-
-    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    @ApiOperation(value = "ログイン")
     public LoginResponse login(@RequestBody LoginRequest request) {
 
         return service.login(request);
