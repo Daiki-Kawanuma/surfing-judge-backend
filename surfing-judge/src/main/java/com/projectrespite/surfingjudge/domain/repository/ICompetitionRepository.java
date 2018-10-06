d69750e65dc7feb192f53fa70074ab7e1da8cdca
Daiki Kawanuma <KAWANUMA@jp.ibm.com>
2018-10-06 16:49:48 +0900

    勝ち上がりUpdateロジック実装

--- surfing-judge/src/main/java/com/projectrespite/surfingjudge/domain/repository/ICompetitionRepository.java
+++ surfing-judge/src/main/java/com/projectrespite/surfingjudge/domain/repository/ICompetitionRepository.java
@@ -3,10 +3,13 @@ package com.projectrespite.surfingjudge.domain.repository;
 import com.projectrespite.surfingjudge.domain.model.data.CompetitionEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ICompetitionRepository {
 
     List<CompetitionEntity> getCompetitionByRound(int round);
     List<CompetitionEntity> getCompetitionByRoundHeat(int round, int heat);
-    void saveCompetitionByRoundHeat(CompetitionEntity entity);
+    Optional<CompetitionEntity> getCompetitionByEntity(CompetitionEntity entity);
+    void save(CompetitionEntity entity);
+    void saveOrUpdate(CompetitionEntity entity);
 }
--- surfing-judge/src/main/java/com/projectrespite/surfingjudge/infrastructure/CompetitionRepository.java
+++ surfing-judge/src/main/java/com/projectrespite/surfingjudge/infrastructure/CompetitionRepository.java
@@ -6,9 +6,11 @@ import com.cloudant.client.api.query.Sort;
 import com.projectrespite.surfingjudge.domain.model.data.CompetitionEntity;
 import com.projectrespite.surfingjudge.domain.repository.ICompetitionRepository;
 import lombok.AllArgsConstructor;
+import lombok.val;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import static com.cloudant.client.api.query.Expression.eq;
 import static com.cloudant.client.api.query.Operation.and;
@@ -42,9 +44,44 @@ public class CompetitionRepository implements ICompetitionRepository {
     }
 
     @Override
-    public void saveCompetitionByRoundHeat(CompetitionEntity entity) {
+    public Optional<CompetitionEntity> getCompetitionByEntity(CompetitionEntity entity) {
+
+        val competitions = client.database("competition", false)
+                .query(new QueryBuilder(and(eq("round", entity.getRound()),
+                        eq("heat", entity.getHeat()),
+                        eq("player_color", entity.getPlayerColor())))
+                        .fields("_id", "_rev", "player_number", "player_name", "player_color", "round", "heat")
+                        .build(), CompetitionEntity.class)
+                .getDocs();
+
+        if(competitions.isEmpty())
+            return Optional.empty();
+        else
+            return Optional.of(competitions.get(0));
+
+    }
+
+    @Override
+    public void save(CompetitionEntity entity) {
 
         client.database("competition", false)
                 .save(entity);
+    }
+
+    @Override
+    public void saveOrUpdate(CompetitionEntity entity) {
+
+        val optional = getCompetitionByEntity(entity);
+
+        if(optional.isPresent()) {
+            entity.setId(optional.get().getId());
+            entity.setRev(optional.get().getRev());
+
+            client.database("competition", false)
+                    .update(entity);
+        } else {
+            client.database("competition", false)
+                    .save(entity);
+        }
     }
 }
